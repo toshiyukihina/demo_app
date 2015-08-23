5c900bfe1223d90be6f8cc63315b700ce4dc0669
Toshiyuki Hina <toshiyuki.hina@gmail.com>
2015-08-23 10:50:41 +0900

    Set the root path

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150823012446) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "microposts", force: true do |t|
     t.string   "content"
     t.integer  "user_id"
